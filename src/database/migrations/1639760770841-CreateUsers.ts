c6acf22a00f6338bb384be58af96e3bc25eedad9
murilo <muriraubach@gmail.com>
2021-12-17 17:13:44 -0300

    refactor: delete username column from users

--- src/database/migrations/1639760770841-CreateUsers.ts
+++ src/database/migrations/1639760770841-CreateUsers.ts
@@ -15,11 +15,6 @@ export class CreateUsers1639760770841 implements MigrationInterface {
             type: "varchar",
           },
           {
-            name: "username",
-            type: "varchar",
-            isUnique: true,
-          },
-          {
             name: "password",
             type: "varchar",
           },
