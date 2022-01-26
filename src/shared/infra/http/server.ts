e4584ed72309b424f7196cf583b116f0135ef9c7
Murilo Vargas <muriraubach@gmail.com>
2022-01-26 19:58:24 -0300

    refactor: move server.ts to shared infra folder

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,11 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "./database";
-import "./shared/container";
-import { AppError } from "./errors/AppError";
+import "../../../database";
+import "../../container";
+import { AppError } from "../../../errors/AppError";
+import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-import swaggerFile from "./swagger.json";
 
 const app = express();
 
