c0a871b290a403184ae5c74f05e3b4d2b80a2ee6
Murilo Vargas <muriraubach@gmail.com>
2022-01-31 17:11:07 -0300

    refactor: update imports

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { CreateCarController } from "../../../../modules/cars/useCases/CreateCar/CreateCarController";
+import { CreateCarController } from "../../../../modules/cars/useCases/createCar/CreateCarController";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
