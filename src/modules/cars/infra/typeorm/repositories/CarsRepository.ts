fc5e15aa14f588ae663d0a0acf3c859da94a83bb
Murilo Vargas <muriraubach@gmail.com>
2022-02-12 18:49:46 -0300

    feat: add list available method in cars repository

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -41,6 +41,27 @@ class CarsRepository implements ICarsRepository {
 
     return car;
   }
+
+  async listAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsQuery = await this.repository
+      .createQueryBuilder("c")
+      .where("available = :available", { available: true });
+
+    if (name) carsQuery.andWhere("name = :name", { name });
+
+    if (brand) carsQuery.andWhere("brand = :brand", { brand });
+
+    if (category_id)
+      carsQuery.andWhere("category_id = :category_id", { category_id });
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
+  }
 }
 
 export { CarsRepository };
