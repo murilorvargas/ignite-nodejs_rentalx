829d28cd62eaac65054e9ecfb60a9539098bd248
Murilo Vargas <muriraubach@gmail.com>
2022-02-20 17:04:17 -0300

    feat: set id column of users table as primary key

--- src/shared/infra/typeorm/migrations/1639760770841-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1639760770841-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1639760770841 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
