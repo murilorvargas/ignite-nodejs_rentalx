350b01b3b9bbf7c32dbec6665bf917e6e69a56da
Murilo Vargas <muriraubach@gmail.com>
2022-02-28 17:46:53 -0300

    feat: add list rentals by user route

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import { CreateRentalController } from "../../../../modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "../../../../modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "../../../../modules/rentals/useCases/listRetalsByUser/ListRentalsByUserController";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
 
@@ -15,6 +17,12 @@ rentalsRoutes.post(
   "/devolution/:id",
   ensureAuthenticated,
   devolutionRentalController.handle
+);
+
+rentalsRoutes.get(
+  "/user",
+  ensureAuthenticated,
+  listRentalsByUserController.handle
 );
 
 export { rentalsRoutes };
