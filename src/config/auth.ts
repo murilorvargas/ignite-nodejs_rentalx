957fc52840465d157882e33a3d12073174b479ac
Murilo Vargas <muriraubach@gmail.com>
2022-03-03 11:23:53 -0300

    feat: create a config auth file

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "3d40c32be616902268fbf12c20f906c3",
+  expires_in: "15m",
+  secret_refresh_token: "de0d9fa2cc47f9bc2c6aed70ee0e26bf",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
