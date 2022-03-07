6118d29368bd85d2c5fccdd6af17dddad6443a5e
Murilo Vargas <muriraubach@gmail.com>
2022-03-07 09:49:37 -0300

    feat: add method compare if before in date provider interface

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
   compareInHours(start_date: Date, end_date: Date): number;
   compareInDays(start_date: Date, end_date: Date): number;
   convertToUTC(date: Date): string;
+  compareIfBefore(start_date: Date, end_date: Date): boolean;
   addDays(days: number): Date;
   addHours(hours: number): Date;
   dateNow(): Date;
